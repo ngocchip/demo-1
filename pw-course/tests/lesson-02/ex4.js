edf51fd189ab799c2582593aee7cb832eee559f8
Ngoc Tran <tranbichngoc305@gmail.com>
2026-06-02 00:37:02 +0700

    feat: add file btvn2

---
+++ pw-course/tests/lesson-02/ex4.js
@@ -0,0 +1,7 @@
+const chieucao = 120;
+
+const cannanglytuong = (chieucao - 100)*9/10;
+const muccantoida = chieucao - 100;
+const mucantoithieu = (chieucao - 100)*8/10;
+
+console.log("Cân nặng lý tưởng:", cannanglytuong + "Mức cân nặng tối đa:", muccantoida + "Mức cân nặng tối thiểu:", mucantoithieu);
